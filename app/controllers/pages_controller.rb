d1a046a96bccb308409a4dc1adba383e1bfb8ea5
ANGUYEN <nguyen.arafath@gmail.com>
2020-10-01 10:35:40 +0200

    Dashboard creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only:  [:dashboard]
   def home
     if current_user
       redirect_to toys_path
     end
+  end
+
+  def dashboard
+    @toys = current_user.toys
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :toys
   devise_for :users 
   devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+    get '/users/sign_out' => 'devise/sessions#destroy'  
+    get 'dashboard' => 'pages#dashboard'   
  end
   
 
