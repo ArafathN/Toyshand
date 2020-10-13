3c6e9b3fb28d0eb42504ba81b2ee3b0402f0b1e7
ANGUYEN <nguyen.arafath@gmail.com>
2020-10-13 11:19:50 +0200

    Added Stripe and transactions controller

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'cancan'
 gem "paperclip", "~> 6.0.0"
 gem 'dotenv-rails', groups: [:development]
 gem 'font_awesome5_rails'
+gem 'stripe'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,31 @@
+class TransactionsController < ApplicationController
+
+    def create
+        toy = toy.find_by!(slug:params[:slug])
+        token = params[:stripeToken]
+    end
+
+    begin
+        charge = Stripe::Charge.create(
+            amount: toy.price
+            currency: "eur"
+            description: current_user.email
+            )
+
+            @sale = toy.sales.create!(buyer_email: current_user.email)
+            redirect_to pickup_url(uuid: @sale.uuid)
+
+        rescue Stripe::CardError => e
+            flash[:notice] = e.message
+            redirect_to toy_path(toy)
+        end
+    end
+
+
+    def pickup
+        @sale = Sale.find_by!(uuid: params[:uuid])
+        @toy = @sale.toy
+    end
+
+
+end
